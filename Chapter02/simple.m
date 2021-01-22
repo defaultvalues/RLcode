b564d445e3210dea47ede9177d50f69cf28a22f1
Chenmin Sha <15962939789@163.com>
2021-01-22 15:55:21 +0800

    modify chapter 02

--- Chapter02/simple.m
+++ Chapter02/simple.m
@@ -6,19 +6,23 @@ Q = zeros(size(mean));
 N = zeros(size(mean));
 e = 0.01;
 value(1) = 0;
-
-for i = 1:200
-  if rand(1) < 1-e
+Choice = zeros(1,20000);
+for i = 2:20000
+    if rand(1) < 1-e
         [~, A] = max(Q);
     else
         A = randi(5,1);
     end
     reward = randn(size(mean))+mean;
+    [~,I] = max(reward);
+    if I == A
+        Choice(i) = Choice(i-1)+1;
+    end
     R = reward(A);
     N(A) = N(A)+1;
     Q(A) = Q(A)+(R-Q(A))/N(A);
-    value(i+1) = value(i)+R;
 end
-
+t = 1:20000;
 figure()
-plot(value)
+plot(Choice);
+disp('Done');
